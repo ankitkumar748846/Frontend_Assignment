417d2ea6756bc850eeee1bf5bd57eb5262e9b4d3
ankitkumar748846 <ankitkumar748846@gmail.com>
2024-09-19 17:26:52 +0530

    feat: NavBar feature implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import { useState} from 'react';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  const [group, setGroup] = useState('status');
+  const [order, setOrder] = useState('priority');
+
+  const handleGroupChange = (groupSelected) => {
+    setGroup(groupSelected);
+    localStorage.setItem("selectedGroup", groupSelected);
+  }
+
+  const handleOrderChange = (orderSelected) => {
+    setOrder(orderSelected);
+    localStorage.setItem("selectedOrder", orderSelected);
+  }
+
   return (
-   <div>Project setup</div>
+    <div className="App scroll-container">
+      <Navbar 
+        group={group} 
+        order={order} 
+        onGroupChange={handleGroupChange} 
+        onOrderChange={handleOrderChange} 
+      />
+    </div>
   );
 }
 
